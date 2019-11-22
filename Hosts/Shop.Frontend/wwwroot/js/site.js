fd4d94a1b805882c81ef0c5e76f4ad123606dbcf
Daniel Olsen <daniel.olsen.server@gmail.com>
2019-11-22 02:39:57 +0100

    Started implementing UI

---
+++ Hosts/Shop.Frontend/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿window.InitializeDropDowns = function () {
+    var options = {
+        alignment: 'bottom',
+        coverTrigger: false,
+        hover: true
+    };
+
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, options);
+};
